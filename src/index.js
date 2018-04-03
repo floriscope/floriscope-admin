ef6d870734e4d38ff15b86df8c50c6a0f67eb5e7
Gaëtan DUHAMEL <gaetan.duhamel@plante-et-cite.fr>
2018-04-03 12:00:03 +0200

    redux-persist-v5 dva integration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import '@babel/polyfill';
 import 'url-polyfill';
 import dva from 'dva';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import createHistory from 'history/createHashHistory';
 // user BrowserHistory
@@ -8,12 +10,41 @@ import createHistory from 'history/createHashHistory';
 import createLoading from 'dva-loading';
 import 'moment/locale/zh-cn';
 import FastClick from 'fastclick';
+import onError from './error';
 import './rollbar';
 
 import './index.less';
+
+// 0. Set peristence layer
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['global', 'user'],
+};
+let $persistor;
+export function createPersistorIfNecessary(store) {
+  if (!$persistor && store) {
+    $persistor = persistStore(store);
+    const rootReducer = persistReducer(persistConfig, state => state);
+    store.replaceReducer(rootReducer);
+    $persistor.persist();
+  }
+  return $persistor;
+}
+
 // 1. Initialize
 const app = dva({
+  onReducer: (reducer) => {
+    if (createPersistorIfNecessary(app._store)) {
+      const newReducer = persistReducer(persistConfig, reducer);
+      setTimeout(() => $persistor && $persistor.persist(), 0);
+      return newReducer;
+    } else {
+      return reducer;
+    }
+  },
   history: createHistory(),
+  onError,
 });
 
 // 2. Plugins
@@ -28,7 +59,6 @@ app.router(require('./router').default);
 // 5. Start
 app.start('#root');
 
-
 FastClick.attach(document.body);
 
-export default app._store;  // eslint-disable-line
+export default persistStore(app._store); // eslint-disable-line
--- src/router.js
+++ src/router.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import { routerRedux, Switch } from 'dva/router';
 import { LocaleProvider, Spin } from 'antd';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import dynamic from 'dva/dynamic';
 import { getRouterData } from './common/router';
 import Authorized from './utils/Authorized';
 import styles from './index.less';
+import { createPersistorIfNecessary } from './index';
 
 const { ConnectedRouter } = routerRedux;
 const { AuthorizedRoute } = Authorized;
-dynamic.setDefaultLoadingComponent(() => {
-  return <Spin size="large" className={styles.globalSpin} />;
-});
+const Loading = <Spin size="large" className={styles.globalSpin} />;
+dynamic.setDefaultLoadingComponent(() => Loading);
 
 function RouterConfig({ history, app }) {
   const routerData = getRouterData(app);
   const UserLayout = routerData['/user'].component;
   const BasicLayout = routerData['/'].component;
   return (
-    <LocaleProvider locale={zhCN}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <AuthorizedRoute
-            path="/user"
-            render={props => <UserLayout {...props} />}
-            authority={['guest', 'reader']}
-            redirectPath="/"
-          />
-          <AuthorizedRoute
-            path="/"
-            render={props => <BasicLayout {...props} />}
-            authority={['admin', 'editor']}
-            redirectPath="/user/login"
-          />
-        </Switch>
-      </ConnectedRouter>
-    </LocaleProvider>
+    <PersistGate persistor={createPersistorIfNecessary(app._store)} loading={Loading}>
+      <LocaleProvider locale={zhCN}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <AuthorizedRoute
+              path="/user"
+              render={props => <UserLayout {...props} />}
+              authority={['guest', 'reader']}
+              redirectPath="/"
+            />
+            <AuthorizedRoute
+              path="/"
+              render={props => <BasicLayout {...props} />}
+              authority={['admin', 'editor']}
+              redirectPath="/user/login"
+            />
+          </Switch>
+        </ConnectedRouter>
+      </LocaleProvider>
+    </PersistGate>
   );
 }
 
